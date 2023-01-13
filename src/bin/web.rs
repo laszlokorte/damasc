3c9cd4ac096cc8fe760769d435386badbe476a9b
Laszlo Korte <me@laszlokorte.de>
2023-01-13 15:25:24 +0100

    cleanup server side web ui and allow running web and cli concurrently

--- src/bin/web.rs
+++ src/bin/web.rs
@@ -1,6 +1,8 @@
 #![cfg(feature = "web")]
 
 use std::env;
+use std::io::Error;
+use std::sync::Arc;
 use std::{collections::BTreeSet, sync::Mutex};
 
 use actix_files::Files;
@@ -14,6 +16,7 @@ use actix_web::{
 use askama::Template;
 use damasc::repl::Repl;
 use damasc::{identifier::Identifier, parser::statement, statement::Statement};
+
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -44,9 +47,11 @@ struct HomeTemplate<'x> {
 #[post("/")]
 async fn eval(
     repl: web::Form<ReplInput>,
-    env_mutex: Data<Mutex<Repl<'_, '_, '_>>>,
+    env_mutex: Data<Arc<Mutex<Repl<'_, '_, '_,'_>>>>,
 ) -> impl Responder {
-    let mut repl_state = env_mutex.lock().unwrap();
+    let Ok(mut repl_state) = env_mutex.lock() else {
+        return HttpResponse::Ok().content_type("text/html").body("Locked");
+    };
 
     let bags = repl_state.bags();
     let vars = repl_state.vars();
@@ -67,10 +72,15 @@ async fn eval(
 
     match statement(&repl.statement) {
         Ok((_, stmt)) => {
-            let deny = matches!(
-                &stmt,
-                Statement::UseBag(..) | Statement::Import(..) | Statement::Export(..)
-            );
+
+            let deny = match &stmt {
+                Statement::UseBag(id,..) => {
+                    !repl_state.bags().contains(id)
+                },
+                Statement::Import(..) => true,
+                Statement::Export(..) => true,
+                _ => false,
+            };
 
             if deny {
                 ResultTemplate {
@@ -147,12 +157,13 @@ struct Configuration {
 async fn main() -> std::io::Result<()> {
     let mut repl = Repl::new("init");
     let Ok((_, stmt)) = statement(".bag jail as _ limit 30") else {
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to parse statement"));
+        return Err(Error::new(std::io::ErrorKind::Other, "Failed to parse statement"));
     };
     let Ok(_) = repl.execute(stmt) else {
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, "Failed to create bag"));
+        return Err(Error::new(std::io::ErrorKind::Other, "Failed to create bag"));
     };
-    let repl_mutex = Data::new(Mutex::new(repl));
+    let repl_mutex = Arc::new(Mutex::new(Repl::new("init")));
+    let repl_mutex_data = Data::new(repl_mutex.clone());
 
     let conf = Configuration {
         ip: env::var("DAMASC_HOST").unwrap_or("127.0.0.1".into()),
@@ -164,10 +175,10 @@ async fn main() -> std::io::Result<()> {
 
     let server = HttpServer::new(move || {
         App::new()
-            .app_data(repl_mutex.clone())
+            .app_data(repl_mutex_data.clone())
             .service(home)
             .service(eval)
-            .service(Files::new("/", "./static/root/").index_file("index.html"))
+            .service(Files::new("/", "./public/"))
             .default_service(web::route().to(not_found))
     })
     .bind((conf.ip, conf.port))?;
@@ -177,6 +188,81 @@ async fn main() -> std::io::Result<()> {
         println!("Listening on {scheme}://{adr}");
     }
 
-    server.run().await
+    let running = server.run();
+    #[cfg(feature = "cli")]
+    {
+        use futures::try_join;
+
+        try_join!(running, cli(repl_mutex.clone()))?;
+        Ok(())
+    }
+
+    #[cfg(not(feature = "cli"))] 
+    {
+        return running.await
+    }
 }
 
+#[cfg(feature = "cli")]
+async fn cli(repl_mutex: Arc<Mutex<Repl<'_, '_, '_, '_>>>) -> Result<(), Error> {
+    use rustyline::Editor;
+    use rustyline::error::ReadlineError;
+    use damasc::repl::ReplError;
+
+    if let Ok(mut rl) = Editor::<()>::new() {
+        if rl.load_history("history.txt").is_err() {
+            println!("No previous history.");
+        }
+    
+
+        println!("Starting REPL because feature 'cli' is enabled.");
+        println!("press CTRL-D to exit.");
+        println!(".bag");
+        if let Ok(repl) = repl_mutex.lock() {
+            println!("Current Bag: {}", repl.current_bag);
+        };
+    
+        loop {
+            let readline = rl.readline(">> ");
+            match readline {
+                Ok(line) => {
+                    rl.add_history_entry(line.as_str());
+                    let input = line.as_str();
+    
+                    let stmt = match statement(input) {
+                        Ok((_, s)) => s,
+                        Err(e) => {
+                            println!("read error: {e}");
+                            continue;
+                        }
+                    };
+
+                    let Ok(mut repl) = repl_mutex.lock() else {
+                        continue;
+                    };
+    
+                    match repl.execute(stmt) {
+                        Ok(r) => {
+                            println!("{r}")
+                        }
+                        Err(ReplError::Exit) => return Err(Error::new(std::io::ErrorKind::BrokenPipe, "Closed by user")),
+                        Err(e) => println!("Error: {e:?}"),
+                    }
+                }
+                Err(ReadlineError::Interrupted) => {
+                    continue;
+                }
+                Err(ReadlineError::Eof) => {
+                    return Err(Error::new(std::io::ErrorKind::BrokenPipe, "Closed by user"))
+                }
+                Err(err) => {
+                    println!("Error: {err}");
+                    return Err(Error::new(std::io::ErrorKind::BrokenPipe, err))
+                }
+            }
+        }
+    }
+
+    println!("foo");
+    Ok(())
+}
